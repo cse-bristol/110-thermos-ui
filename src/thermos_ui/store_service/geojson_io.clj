b4fe1cb4b2af5e4195561dbe2af7e4e7f6c31d03
trickyBytes <richard.tiffin@akoolla.com>
2018-01-31 08:34:59 +0000

    Still a noddy implementation of connection service url, just to test against Glenns leaflet demo..doesn't work...

--- src/thermos_ui/store_service/geojson_io.clj
+++ src/thermos_ui/store_service/geojson_io.clj
@@ -1,18 +1,42 @@
-(ns thermos-ui.store-service.geojson-io
-   (:import
-    [org.geotools.data FileDataStoreFinder DataUtilities]
-    [org.geotools.data.collection ListFeatureCollection]
-    [org.geotools.feature.simple SimpleFeatureBuilder]
-    [org.geotools.geojson.feature FeatureJSON]
-    [org.geotools.geojson.geom GeometryJSON]))
+(ns thermos-ui.store-service.geojson-io)
 
 ;;https://stackoverflow.com/questions/34934948/leaflet-and-geojson#34935345
+
+(def eg-1
+  {"type" "FeatureCollection"
+   "features" [{"type" "Feature"
+                "geometry" {"type" "LineString"
+                            "coordinates" [[-0.1243616,51.5535153]
+                                           [-0.1245374,51.5536036]]}
+                "properties" {"id" "e924b72772565009512917a8ef2799bc"
+                              "osm_id" 206103256
+                              "class" "service"
+                              "node_from" "feb3a9fbe2087b3edefad3df7c4a7d48"
+                              "node_to" "cae7ffe65ea2ff823ddec41b8ca4be25"
+                              "name" "*** Unknown ****"
+                              "length" 15.624962}}]})
+(def eg-2
+   {"type" "FeatureCollection"
+   "features" [
+               {"type" "Feature"
+                "geometry" {"type" "LineString"
+                            "coordinates" [[-0.091698646481862,51.5474746364529]
+                                           [-0.091645265151595,51.5474308063962]]}
+                "properties" {"id" "AB4D9716-082B-9F64-2943-342AE672F1B6"
+                              "name" "wewe"}}]})
+  
 (defn connections>geojson
   [connections]
-  ;connections)
-  {:type "FeatureCollection"
-   :features [(let [x (first connections)]
-                {:type "Feature"
-                 :geometry (:geometry x)
-                 :properties {:id (:id x)
-                               :name (:distname x)}})]})
+   {"type" "FeatureCollection"
+   "features" [{"type" "Feature"
+                "geometry" {"type" "LineString"
+                            "coordinates" [[51.5535153 -0.1243616]
+                                           [51.5536036 -0.1245374]]}
+                "properties" {"id" "e924b72772565009512917a8ef2799bc"
+                              "osm_id" 206103256
+                              "class" "service"
+                              "node_from" "feb3a9fbe2087b3edefad3df7c4a7d48"
+                              "node_to" "cae7ffe65ea2ff823ddec41b8ca4be25"
+                              "name" "*** Unknown ****"
+                              "length" 15.624962}}]})
+ 
