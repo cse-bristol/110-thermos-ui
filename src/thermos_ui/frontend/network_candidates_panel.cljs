fd3c5cf2db5ba7b06fee68a1dbe23af14d560ef9
mark <markgibbonsdeveloper@gmail.com>
2018-03-12 11:47:19 +0000

    Add checkbox to "Selected" column

--- src/thermos_ui/frontend/network_candidates_panel.cljs
+++ src/thermos_ui/frontend/network_candidates_panel.cljs
@@ -1,15 +1,15 @@
 (ns thermos-ui.frontend.network-candidates-panel
   (:require [reagent.core :as reagent]
             [thermos-ui.specs.candidate :as candidate]
+            [thermos-ui.frontend.editor-state :as state]
             [thermos-ui.frontend.operations :as operations]
             [thermos-ui.frontend.virtual-table :as virtual-table]))
 
-(declare component get-row-function)
+(declare component selected-cell-renderer)
 
 (defn component
   "DOCSTRING"
   [document]
-  (.log js/console (clj->js (first (operations/constrained-candidates @document))))
   [:div {:style {:height "100%"}}
 
    ;; TODO this will rerun whenever we modify anything in document we
@@ -19,6 +19,33 @@
      [virtual-table/component
       {:items items}
       ;; columns
-      {:label "ID" :key ::candidate/id}
+      {:label "Selected"
+       :key ::candidate/selected
+       :cellRenderer (fn [args] (selected-cell-renderer document args))}
+      {:label "Address" :key ::candidate/name}
+      {:label "Type"
+       :key ::candidate/type
+       :cellRenderer (fn [arg]
+                       (let [cell-value (.-cellData arg)]
+                         (name cell-value)))}
       {:label "Postcode" :key ::candidate/postcode}
       ])])
+
+(defn selected-cell-renderer
+  "Custom cell renderer for `Selected` column.
+  Puts in a checkbox allowing you to (de)select candidates."
+  [document args]
+  (let [is-selected (if args (.-cellData args) false)
+        candidate-id (if args (::candidate/id (.. args -rowData)) false)]
+    (reagent/as-element [:input
+                         {:type "checkbox"
+                          :default-checked is-selected
+                          :on-change (fn [e]
+                                       (if (.. e -target -checked)
+                                         (state/edit! document
+                                                      operations/select-candidates
+                                                      [candidate-id]
+                                                      :union)
+                                         (state/edit! document
+                                                      operations/deselect-candidates
+                                                      [candidate-id])))}])))
