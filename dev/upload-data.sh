9b09651dafe5c550f50153c9b70d4b35a2ea7799
Tom Hinton <tom.hinton@cse.org.uk>
2018-04-26 09:37:10 +0100

    add upload script, remove useless underscores

---
+++ dev/upload-data.sh
@@ -0,0 +1,2 @@
+curl http://localhost:8080/api/map/candidates -XPOST -F file=@alba-uilia-romania-buildings.geojson
+curl http://localhost:8080/api/map/candidates -XPOST -F file=@alba-uilia-romania-ways.geojson
