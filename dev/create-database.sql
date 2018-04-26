80052cba7ad27967209c1c03ed1d1b239c740be5
Tom Hinton <tom.hinton@cse.org.uk>
2018-04-26 09:33:34 +0100

    Update dev server stuff

--- dev/create-database.sql
+++
@@ -1,5 +0,0 @@
-CREATE ROLE postgres LOGIN;
-GRANT root TO postgres;
-ALTER USER postgres WITH PASSWORD 'therm0s';
-
-CREATE DATABASE thermos_geometries;
--- dev/pg-server.nix
+++ dev/pg-server.nix
@@ -5,47 +5,52 @@
 # nixops deploy -d pg-server
 
 {
-  pg-server = {cfg, pkgs, ...}:
-  let pg = pkgs.postgresql;
-      pgis = pkgs.postgis.override { postgresql = pg; } ;
+  network.description = "THERMOS dev database server";
+
+  thermos-app = {pkgs, config, ...} :
+  let
+    pg = pkgs.postgresql;
+  pgis = pkgs.postgis.override { postgresql = pg; };
+  create-database = builtins.toFile "create-database.sql"
+  ''
+    CREATE ROLE postgres LOGIN;
+    GRANT root TO postgres;
+    ALTER USER postgres WITH PASSWORD 'therm0s';
+    CREATE DATABASE thermos_geometries;
+  '';
+  enable-postgis = builtins.toFile "enable-postgis.sql"
+    "CREATE EXTENSION postgis;";
   in
   {
-    deployment.targetEnv = "virtualbox";
+    deployment.targetEnv = "container";
 
     environment.systemPackages = [pkgs.rxvt_unicode.terminfo];
 
-    networking.firewall.allowedTCPPorts = [22 5432];
+    networking.firewall.allowedTCPPorts = [ 22 5432 ];
 
     services.postgresql = {
       enable = true;
       package = pg;
       extraPlugins = [ pgis.v_2_3_1 ];
       enableTCPIP = true;
-      # Terrible, allow anyone to login
-      # as any user over TCPIP
-      # https://www.postgresql.org/docs/devel/static/auth-pg-hba-conf.html
+
       authentication = ''
         local all all trust
         host all all all trust
       '';
 
-      initialScript = ./create-database.sql;
+      initialScript = create-database;
     };
 
-    systemd.services.populate-db = {
-      path = with pkgs; [
-        gdal pg
-      ];
+    systemd.services.enable-postgis = {
+      path = [pg];
       wantedBy = ["multi-user.target"];
       after = ["postgresql.service"];
       requires = ["postgresql.service"];
-      script =
-      ''
-        [[ -f /var/lib/populated-db ]] && exit 0
-
-        psql -U root -d thermos_geometries -a -f "${./enable-postgis.sql}"
-
-        touch /var/lib/populated-db
+      script = ''
+        [[ -f /var/lib/enabled-postgis ]] ||
+        ( psql -U postgres -d thermos_geometries -a -f "${enable-postgis}" &&
+          touch /var/lib/enabled-postgis )
       '';
     };
   };
