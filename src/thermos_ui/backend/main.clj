fa7d33110dc8a09e72417caff571a10feabaf86f
Tom Hinton <tom.hinton@cse.org.uk>
2018-04-27 16:48:28 +0100

    Handle large file uploads a bit better

--- src/thermos_ui/backend/main.clj
+++ src/thermos_ui/backend/main.clj
@@ -5,5 +5,7 @@
   (:gen-class))
 
 (defn -main [& args]
-  (run-server #'app {:max-body (* 8388608 100) ;; 800mega
+  (run-server #'app {:max-body (* 8388608 200)  ;; 800mega
                      :port (Integer/parseInt (config :server-port)) :join? true}))
+
+
--- src/thermos_ui/backend/maps/routes.clj
+++ src/thermos_ui/backend/maps/routes.clj
@@ -1,7 +1,11 @@
 (ns thermos-ui.backend.maps.routes
   (:require [compojure.core :refer :all]
             [thermos-ui.backend.maps.db :as db]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [ring.util.io :as ring-io]
+            )
+  
   )
 
 (defn- as-int [s] (Integer/parseInt (re-find #"\d+" s)))
@@ -14,16 +18,35 @@
 (defroutes map-data-routes
   (GET "/map/candidates/:zoom/:x-tile/:y-tile"
        [zoom x-tile y-tile]
-       (let [candidates (db/find-tile (as-int zoom) (as-int x-tile) (as-int y-tile))]
+       (if (< 11 zoom)
          {:status 200
           :body {:type :FeatureCollection
-                 :features (map candidate->feature candidates)}}
-         ))
+                 :features []}}
+         (let [candidates (db/find-tile (as-int zoom) (as-int x-tile) (as-int y-tile))]
+           {:status 200
+            :body {:type :FeatureCollection
+                   :features (map candidate->feature candidates)}}
+           ))
+       )
 
   (POST "/map/candidates" {{data :file} :params}
-       ;; add some candidates
+        ;; add some candidates
         (if-let [data (data :tempfile)]
-          (if (db/insert! data)
-            {:status 200})))
+          {:status 200
+           :body (ring-io/piped-input-stream
+                  (fn [ostream]
+                    (try (with-open [w (io/writer ostream)]
+                           (db/insert!
+                            data
+                            #(do (.write w (str % "\n"))
+                                 (.flush w)
+                                 (.flush ostream))))
+                         (catch Exception e
+                           (println "Exception in insert"
+                                    e)
+                           )
+                         )))
+           }
+          )))
 
-  )
+  
