e8766dff4001e202efa1079bb9f10cd8d1577c38
Tom Hinton <tom.hinton@cse.org.uk>
2018-11-23 13:48:01 +0000

    indentation

--- src/thermos_frontend/inputs.cljs
+++ src/thermos_frontend/inputs.cljs
@@ -13,36 +13,36 @@
 (defn number [{value-atom :value-atom scale :scale step :step :as ks}]
   (reagent/with-let [element (atom nil)]
     (let [scale (or scale 1)
-         step  (or step 1)
+          step  (or step 1)
 
-         digits (max 0 (- (Math/log10 step)))
-         
-         s-value (.toFixed
-                  (* scale (or (when value-atom @value-atom) (:value ks)))
-                  digits)
+          digits (max 0 (- (Math/log10 step)))
+          
+          s-value (.toFixed
+                   (* scale (or (when value-atom @value-atom) (:value ks)))
+                   digits)
           on-change (or (:on-change ks) (partial reset! value-atom))
           parse (if (integer? (or (:step ks) 1))
-                            js/parseInt
-                            js/parseFloat)
+                  js/parseInt
+                  js/parseFloat)
           ]
-     
-     [:input.input
-      (merge {:type :number
-              :placeholder "0"
-              :default-value s-value}
-             
-             (dissoc ks :value-atom :scale)
-             {:ref #(reset! element %)
-
-              :on-blur
-              #(let [val @value-atom]
-                 (set! (.. @element -value)
-                       (* val scale)))
+      
+      [:input.input
+       (merge {:type :number
+               :placeholder "0"
+               :default-value s-value}
               
-              :on-change
-              #(let [val (.. % -target -value)
-                     val (/ (parse val) scale)]
-                 (on-change val))})])))
+              (dissoc ks :value-atom :scale)
+              {:ref #(reset! element %)
+
+               :on-blur
+               #(let [val @value-atom]
+                  (set! (.. @element -value)
+                        (* val scale)))
+               
+               :on-change
+               #(let [val (.. % -target -value)
+                      val (/ (parse val) scale)]
+                  (on-change val))})])))
 
 (defn select [{value-atom :value-atom values :values}]
   [:select
