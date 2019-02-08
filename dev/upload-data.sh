356ee7317f17421162febaaecfc4c18abf128f03
Tom Hinton <tom.hinton@cse.org.uk>
2019-02-08 17:01:51 +0000

    Redo build process as a pair of clojure scripts

--- dev/upload-data.sh
+++
@@ -1,2 +0,0 @@
-curl http://localhost:8080/api/map/candidates -XPOST -F file=@alba-iulia-romania-buildings.geojson
-curl http://localhost:8080/api/map/candidates -XPOST -F file=@alba-iulia-romania-ways.geojson
