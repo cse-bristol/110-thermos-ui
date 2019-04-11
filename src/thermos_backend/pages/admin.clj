a0beabd8920b13b0281101e81642c6156823bcc4
Tom Hinton <tom.hinton@cse.org.uk>
2019-04-11 13:49:23 +0100

    space between links

--- src/thermos_backend/pages/admin.clj
+++ src/thermos_backend/pages/admin.clj
@@ -28,9 +28,9 @@
       [:div.flex-cols
        [:h1.flex-grow q " tasks"]
        [:a {:href (str "clean-queue/" q)} "clean up"]
-       " "
-       [:a {:href (str "clean-queue/" q "?purge=1")} "purge"]
-       ]
+       " • "
+       [:a {:href (str "clean-queue/" q "?purge=1")} "purge"]]
+      
       [:table
        [:thead [:tr [:th "ID"] [:th "State"] [:th "Queued"] [:th "Updated"]]]
        
