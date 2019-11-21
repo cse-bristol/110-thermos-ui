6cc64ae4dcdd78cbc4d5d9d12fb9ce66cb30b5ba
mark <mg8404@my.bristol.ac.uk>
2019-11-21 17:25:50 +0000

    Make map legend

--- src/thermos_frontend/view_control.cljs
+++ src/thermos_frontend/view_control.cljs
@@ -6,7 +6,8 @@
 (defn- component [doc]
   (reagent/with-let
     [view (reagent/cursor doc [::view/view-state ::view/map-view])
-     show-pipe-diameters? (reagent/cursor doc [::view/view-state ::view/show-pipe-diameters])]
+     show-pipe-diameters? (reagent/cursor doc [::view/view-state ::view/show-pipe-diameters])
+     legend-visible? (reagent/atom false)]
     [:div.view-control
      [:h2.view-control__label "Map view:"]
      [:button.view-control__button
@@ -25,4 +26,24 @@
           :checked (boolean @show-pipe-diameters?)
           :on-change (fn [] (state/edit! doc view/toggle-show-pipe-diameters))}]
         "Display relative pipe diameters"])
+     [:span.pull-right
+      [:button.view-control__button
+       {:class (when @legend-visible? "view-control__button--active")
+        :on-click #(swap! legend-visible? not)}
+       "Map legend"]
+      (when @legend-visible?
+        [:div.map-legend
+         (if (= ::view/constraints @view)
+           [:dl
+            [:div [:dt.key.required-key] [:dd "Required"]]
+            [:div [:dt.key.optional-key] [:dd "Optional"]]
+            [:div [:dt.key.forbidden-key] [:dd "Forbidden"]]
+            [:div [:dt.key.supply-key] [:dd "Network supply"]]]
+           ;; Solution
+           [:dl
+            [:div [:dt.key.in-network-key] [:dd "In network"]]
+            [:div [:dt.key.not-in-network-key] [:dd "Not in network"]]
+            [:div [:dt.key.unreachable-key] [:dd "Cannot be reached by network"]]
+            [:div [:dt.key.alternative-key] [:dd "Has alternative"]]
+            [:div [:dt.key.supply-key] [:dd "Network supply"]]])])]
      ]))
