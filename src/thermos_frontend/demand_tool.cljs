4b4a71571b9d30e92bdefa457a673804c3d3cc1c
Tom Hinton <tom.hinton@cse.org.uk>
2019-11-27 16:59:24 +0000

    Add default demand figures to new buildings (required)

--- src/thermos_frontend/demand_tool.cljs
+++ src/thermos_frontend/demand_tool.cljs
@@ -8,6 +8,7 @@
 
             [thermos-specs.document :as document]
             [thermos-specs.candidate :as candidate]
+            [thermos-specs.demand :as demand]
             ))
 
 (def start-state {::drawing false})
@@ -80,6 +81,8 @@
              ::candidate/selected true
              ::spatial/jsts-geometry geometry
              ::candidate/inclusion :optional
+             ::demand/kwh 0
+             ::demand/kwp 0
              }
             ]
         (editor-state/edit-geometry!
