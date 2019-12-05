f9cddc0e232e76572c06f2eb3e91408c18ad61a4
Tom Hinton <tom.hinton@cse.org.uk>
2019-12-05 20:33:59 +0000

    Add spinner for loading state

--- src/thermos_backend/pages/editor.clj
+++ src/thermos_backend/pages/editor.clj
@@ -4,6 +4,8 @@
             [hiccup.util :refer [raw-string]]
             [hiccup.page :refer [include-js include-css]]
             [ring.util.anti-forgery :as anti-forgery]
+            [thermos-pages.spinner :as spinner]
+            [thermos-pages.common :refer [style]]
             [thermos-backend.pages.common :refer [source-sans-pro preloaded-values]]))
 
 (defn editor-page [name
@@ -22,7 +24,13 @@
      (include-css "/css/editor.css" source-sans-pro)]
     
    [:body
-    [:div#app
-     [:h1 "Loading, please wait"]]
+    [:div#app {:style (style :background :white)}
+     [:div
+      {:style (style :position :absolute
+                     :top :50% :left :50%
+                     :transform "translate(-50%, -50%)"
+                     )}
+      (spinner/spinner {:size 128})]
+     ]
     (include-js "/js/editor.js")])))
 
