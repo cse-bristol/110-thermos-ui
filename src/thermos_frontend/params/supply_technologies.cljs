0750321613e3a5e116266627d80d44fa0f018fb2
Tom Hinton <tom.hinton@cse.org.uk>
2020-05-29 17:00:49 +0100

    Tidy up style a bit

--- src/thermos_frontend/params/supply_technologies.cljs
+++ src/thermos_frontend/params/supply_technologies.cljs
@@ -5,7 +5,8 @@
             [thermos-frontend.debug-box :as debug]
             [thermos-specs.supply :as supply]
             [clojure.spec.alpha :as s]
-            [thermos-pages.symbols :as syms]))
+            [thermos-pages.symbols :as syms]
+            [clojure.set :as set]))
 
 (def empty-cost {:fixed 0 :per-kwh 0 :per-kwp 0})
 
@@ -36,15 +37,18 @@
    [:td {:key :fixed}
     [inputs/number
      {:value-atom (reagent/cursor cost-atom [:fixed])
-      :min 0 :max 1000 :scale (/ 1 1000)}]]
+      :style {:width :4em}
+      :min 0 :max 1000 :scale (/ 1000.0)}]]
    [:td {:key :capacity}
     [inputs/number
      {:value-atom (reagent/cursor cost-atom [:per-kwp])
+      :style {:width :4em}
       :min 0 :max 1000}
      ]]
    [:td {:key :output}
     [inputs/number
      {:value-atom (reagent/cursor cost-atom [:per-kwh])
+      :style {:width :4em}
       :min 0 :max 1000
       }]]))
 
@@ -55,18 +59,17 @@
                      fuel-options (reagent/track
                                    #(let [fuels (::supply/fuels @doc)]
                                       (sort (for [[k {n :name}] fuels]
-                                              [k n]))))]
+                                              [k n]))))
+
+
+                     focus-next (reagent/atom nil)
+                     ]
     
     [:div.card.flex-grow.parameters-component
      [:h1 "Supply technologies"]
      [:div {:style {:overflow-x :auto}}
       [:table 
        [:thead
-        [:tr
-         [:th {:col-span 8}]
-         [:th {:col-span 3} "Capital cost"]
-         [:th {:col-span 3} "Operating cost"]
-         ]
         [:tr
          [:th]
          [:th "Technology"]
@@ -77,13 +80,27 @@
          [:th "Power/fuel"]
          [:th "Heat/fuel"]
          [:th "Substation"]
+         [:th {:col-span 3} "Capital cost"]
+         [:th {:col-span 3} "Operating cost"]
+         ]
+        [:tr
+         [:th ]
+         [:th ]
+         [:th "yr"]
+         [:th ]
+         [:th ]
+         [:th "MW"]
+         [:th "%"]
+         [:th "%"]
+         [:th ]
          [:th "k¤"]
          [:th "¤/kWp"]
          [:th "¤/kWh"]
          [:th "k¤"]
          [:th "¤/kWp"]
          [:th "¤/kWh"]
-         ]]
+         ]
+        ]
        [:tbody
         (doall
          (for [[plant-option params] (sort-by first @plant-options)]
@@ -93,8 +110,12 @@
             [:td [inputs/text {:value-atom (reagent/cursor plant-options [plant-option :name])
                                :placeholder "Plant type"
                                :style {:width :8em}
-                               }
-                  ]
+                               :ref
+                               (fn [e]
+                                 (when (= [:plant plant-option]
+                                          @focus-next)
+                                   (reset! focus-next nil)
+                                   (when e (.focus e))))}]
              ]
             [:td [inputs/number {:value-atom (reagent/cursor plant-options [plant-option :lifetime])
                                  :style {:width :3em}
@@ -107,11 +128,14 @@
                    :value (-> params :chp)}]]
             [:td [inputs/number
                   {:value-atom (reagent/cursor plant-options [plant-option :capacity-kwp])
-                   :step 1
+                   :style {:width :5em}
+                   :step 0.1
+                   :scale (/ 1000.0)
                    :min 1 :max 1000}]]
             [:td (if (:chp params)
                    [inputs/number
                     {:value-atom (reagent/cursor plant-options [plant-option :power-efficiency])
+                     :style {:width :5em}
                      :scale 100
                      :step 0.1
                      :min 1 :max 100}]
@@ -119,6 +143,7 @@
                    )]
             [:td [inputs/number
                   {:value-atom (reagent/cursor plant-options [plant-option :heat-efficiency])
+                   :style {:width :5em}
                    :scale 100
                    :step 0.1
                    :min 1 :max 100}]]
@@ -136,29 +161,43 @@
             (cost-cells
              (reagent/cursor plant-options [plant-option :operating-cost]))
             ]))
-        [:tr
-         [:td]
-         [:td 
-          [:input {:type :text :placeholder "New plant option"
-                   :style {:width :100%}
-                   :on-key-up
-                   #(when (= (.-key %) "Enter")
-                      (let [pname (.. % -target -value)]
-                        (swap! doc supply/add-plant (new-plant pname))))}]]]]
+        ]
+       ]]
+     [:div {:style {:margin :1em}}
+      [:button.button
+       {:on-click
+        #(let [ids (set (keys @plant-options))
+               new-ids (-> (swap! doc supply/add-plant (new-plant ""))
+                           (::supply/plants)
+                           (keys)
+                           (set))
+               new-id (first (set/difference new-ids ids))]
+           (when new-id
+             (reset! focus-next [:plant new-id])))}
+       "Add plant"
        ]]
 
      [:h1 "Substations"]
      [:div {:style {:overflow-x :auto}}
       [:table
        [:thead
-        [:tr [:th] [:th "Name"] [:th "Headroom"] [:th "Alpha"]]]
+        [:tr [:th] [:th "Name"] [:th "Headroom"] [:th "Alpha"]]
+        [:tr [:th] [:th ] [:th "MW"] [:th "%"]]]
        
        [:tbody
         (for [[id params] (sort-by first @substations)]
           [:tr {:key id}
            [:td [delete-button {:on-click
                                 #(swap! doc supply/remove-substation id)}]]
-           [:td [inputs/text {:value-atom (reagent/cursor substations [id :name]) :placeholder "Name"}]]
+           [:td [inputs/text {:value-atom (reagent/cursor substations [id :name])
+                              :placeholder "Name"
+                              :ref (fn [e]
+                                     (when (= [:substation id]
+                                              @focus-next)
+                                       (reset! focus-next nil)
+                                       (when e (.focus e))))
+                              
+                              }]]
            [:td [inputs/number
                  {:value-atom (reagent/cursor substations [id :headroom-kwp])
                   :scale (/ 1 1000.0)
@@ -169,67 +208,97 @@
                   :scale 100
                   :step 0.1
                   :min 1 :max 100}]]
-           ]
-          )
-        [:tr [:td]
-         [:td
-          [:input {:type :text :placeholder "New substation"
-                   :style {:width :100%}
-                   :on-key-up
-                   #(when (= (.-key %) "Enter")
-                      (let [pname (.. % -target -value)]
-                        (swap! doc supply/add-substation (new-substation pname))))}]]]
-        ]]]
+           ])]
+       ]]
+     [:div {:style {:margin :1em}}
+      [:button.button
+       {:on-click
+        #(let [ids (set (keys @substations))
+               new-ids (-> (swap! doc supply/add-substation (new-substation ""))
+                           (::supply/substations)
+                           (keys)
+                           (set))
+               new-id (first (set/difference new-ids ids))]
+           (when new-id
+             (reset! focus-next [:substation new-id])))}
+       "Add substation"
+       ]]
+     
      
      [:h1 "Storage technologies"]
      [:div {:style {:overflow-x :auto}}
       [:table
        [:thead
-        [:tr [:th {:col-span 4}] [:th {:col-span 3} "Capital cost"]]
         [:tr
          [:th]
          [:th "Name"]
          [:th "Lifetime"]
-         [:th "Capacity (kWh)"]
-         [:th "Capacity (kWp)"]
-         [:th "Cycle efficiency"]
-         [:th "k¤"] [:th "¤/kWp"] [:th "¤/kWh"]]]
+         [:th {:col-span 2} "Capacity"]
+         [:th "Efficiency"]
+         [:th {:col-span 3} "Capital cost"]
+         ]
+        [:tr
+         [:th]
+         [:th]
+         [:th "yr"]
+         [:th "MWh"]
+         [:th "MW"]
+         [:th "%"]
+         [:th "k¤"] [:th "¤/kWp"] [:th "¤/kWh"]
+         ]
+        ]
        
        [:tbody
         (for [[id params] (sort-by first @storage-options)]
           [:tr {:key id}
            [:td [delete-button {:on-click #(swap! doc supply/remove-storage id)}]]
-           [:td [inputs/text {:value-atom (reagent/cursor storage-options [id :name]) :placeholder "Name"}]]
+           [:td [inputs/text {:value-atom (reagent/cursor storage-options [id :name])
+                              :placeholder "Name"
+                              :ref (fn [e]
+                                     (when (= [:storage id]
+                                              @focus-next)
+                                       (reset! focus-next nil)
+                                       (when e (.focus e))))
+                              }]]
            [:td [inputs/number {:value-atom (reagent/cursor storage-options [id :lifetime])
+                                :style {:width :3em}
                                 :min 1 :max 100}]]
            [:td [inputs/number
                  {:value-atom (reagent/cursor storage-options [id :capacity-kwh])
-                  :step 1
-                  :min 1 :max 1000000}]]
+                  :step 0.1
+                  :scale (/ 1000.0)
+                  :style {:width :4em}
+                  :min 0.1 :max 1000000}]]
            [:td [inputs/number
                  {:value-atom (reagent/cursor storage-options [id :capacity-kwp])
-                  :step 1
-                  :min 1 :max 10000}]]
+                  :scale (/ 1000.0)
+                  :step 0.1
+                  :style {:width :4em}
+                  :min 0.1 :max 10000}]]
            [:td [inputs/number
                  {:value-atom (reagent/cursor storage-options [id :efficiency])
+                  :style {:width :3em}
                   :scale 100
                   :step 0.1
                   :min 1 :max 100}]]
            (cost-cells
             (reagent/cursor storage-options [id :capital-cost]))
-           ]
-
-          )
-
-        [:tr [:td]
-         [:td
-          [:input {:type :text :placeholder "New storage technology"
-                   :style {:width :100%}
-                   :on-key-up
-                   #(when (= (.-key %) "Enter")
-                      (let [pname (.. % -target -value)]
-                        (swap! doc supply/add-storage (new-storage pname))))}]]]
-        ]]]
+           ])]]
+      
+      [:div {:style {:margin :1em}}
+       [:button.button
+        {:on-click
+         #(let [ids (set (keys @plant-options))
+                new-ids (-> (swap! doc supply/add-storage (new-storage ""))
+                            (::supply/storages)
+                            (keys)
+                            (set))
+                new-id (first (set/difference new-ids ids))]
+            (when new-id
+              (reset! focus-next [:storage new-id])))}
+        "Add storage"
+        ]]
+      ]
      
      
      ])
