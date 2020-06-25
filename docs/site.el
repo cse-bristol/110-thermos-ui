6464e8c83facfb013fae9c715c5136a6cb8646ea
Tom Hinton <tom.hinton@cse.org.uk>
2020-06-25 16:01:49 +0100

    Update docs

--- docs/site.el
+++ docs/site.el
@@ -1,3 +1,7 @@
+(require 'org)
+
+(defun y-or-n-p (&rest) t)
+
 (defun org-latex--skip-video (o link info)
   (let* ((path (let ((raw-path (org-element-property :path link)))
 		 (if (not (file-name-absolute-p raw-path)) raw-path
@@ -87,73 +91,74 @@ attributes are not supported \(see `org-export-read-attribute')."
          )))
   (org-publish-project "docs" t))
 
-(let ((org-export-with-date nil)
-      (org-latex-packages-alist
-       '(("" "fullpage" nil) ("" "parskip" nil))
-       )
-      ;; need to skip wemb files in pdfs
-      (org-publish-project-alist
-       `(("docs"
-          :time-stamp-file nil
-          :base-directory ,default-directory
-          :publishing-directory "/home/hinton/tmp/thermos-pdfs"
-          :recursive t
-          :publishing-function org-latex-publish-to-pdf)
-         )))
-  (org-publish-project "docs" t))
+(when nil
+  (let ((org-export-with-date nil)
+        (org-latex-packages-alist
+         '(("" "fullpage" nil) ("" "parskip" nil))
+         )
+        ;; need to skip wemb files in pdfs
+        (org-publish-project-alist
+         `(("docs"
+            :time-stamp-file nil
+            :base-directory ,default-directory
+            :publishing-directory "/home/hinton/tmp/thermos-pdfs"
+            :recursive t
+            :publishing-function org-latex-publish-to-pdf)
+           )))
+    (org-publish-project "docs" t))
 
 
-(defun org-odt-publish-to-odt (plist filename pub-dir)
-   "Publish an org file to ODT.
+  (defun org-odt-publish-to-odt (plist filename pub-dir)
+    "Publish an org file to ODT.
  
  FILENAME is the filename of the Org file to be published.  PLIST
  is the property list of the given project.  PUB-DIR is the publishing
  directory.
  
  Return output file name."
-   (unless (or (not pub-dir) (file-exists-p pub-dir)) (make-directory pub-dir t))
-   ;; Check if a buffer visiting FILENAME is already open.
-   (let* ((org-inhibit-startup t)
-          (visiting (find-buffer-visiting filename))
-          (work-buffer (or visiting (find-file-noselect filename))))
-     (unwind-protect
-     (with-current-buffer work-buffer
-       (let ((outfile (org-export-output-file-name ".odt" nil pub-dir)))
-         (org-odt--export-wrap
-          outfile
-          (let* ((org-odt-embedded-images-count 0)
-                 (org-odt-embedded-formulas-count 0)
-                 (org-odt-object-counters nil)
-                 (hfy-user-sheet-assoc nil))
-            (let ((output (org-export-as 'odt nil nil nil
-                                         (org-combine-plists
-                                          plist
-                                          `(:crossrefs
-                                            ,(org-publish-cache-get-file-property
-                                              (expand-file-name filename) :crossrefs nil t)
-                                            :filter-final-output
-                                            (org-publish--store-crossrefs
-                                             org-publish-collect-index
-                                             ,@(plist-get plist :filter-final-output))))))
-                  (out-buf (progn (require 'nxml-mode)
-                                  (let ((nxml-auto-insert-xml-declaration-flag nil))
-                                    (find-file-noselect
-                                     (concat org-odt-zip-dir "content.xml") t)))))
-              (with-current-buffer out-buf (erase-buffer) (insert output))))))))
-     (unless visiting (kill-buffer work-buffer))))
+    (unless (or (not pub-dir) (file-exists-p pub-dir)) (make-directory pub-dir t))
+    ;; Check if a buffer visiting FILENAME is already open.
+    (let* ((org-inhibit-startup t)
+           (visiting (find-buffer-visiting filename))
+           (work-buffer (or visiting (find-file-noselect filename))))
+      (unwind-protect
+          (with-current-buffer work-buffer
+            (let ((outfile (org-export-output-file-name ".odt" nil pub-dir)))
+              (org-odt--export-wrap
+               outfile
+               (let* ((org-odt-embedded-images-count 0)
+                      (org-odt-embedded-formulas-count 0)
+                      (org-odt-object-counters nil)
+                      (hfy-user-sheet-assoc nil))
+                 (let ((output (org-export-as 'odt nil nil nil
+                                              (org-combine-plists
+                                               plist
+                                               `(:crossrefs
+                                                 ,(org-publish-cache-get-file-property
+                                                   (expand-file-name filename) :crossrefs nil t)
+                                                 :filter-final-output
+                                                 (org-publish--store-crossrefs
+                                                  org-publish-collect-index
+                                                  ,@(plist-get plist :filter-final-output))))))
+                       (out-buf (progn (require 'nxml-mode)
+                                       (let ((nxml-auto-insert-xml-declaration-flag nil))
+                                         (find-file-noselect
+                                          (concat org-odt-zip-dir "content.xml") t)))))
+                   (with-current-buffer out-buf (erase-buffer) (insert output))))))))
+      (unless visiting (kill-buffer work-buffer))))
 
-(let ((org-export-with-date nil)
-      (org-odt-preferred-output-format "docx")
-      (org-latex-packages-alist
-       '(("" "fullpage" nil) ("" "parskip" nil))
-       )
-      ;; need to skip wemb files in pdfs
-      (org-publish-project-alist
-       `(("docs"
-          :time-stamp-file nil
-          :base-directory ,default-directory
-          :publishing-directory "/home/hinton/tmp/thermos-pdfs"
-          :recursive t
-          :publishing-function org-odt-publish-to-odt)
-         )))
-  (org-publish-project "docs" t))
+  (let ((org-export-with-date nil)
+        (org-odt-preferred-output-format "docx")
+        (org-latex-packages-alist
+         '(("" "fullpage" nil) ("" "parskip" nil))
+         )
+        ;; need to skip wemb files in pdfs
+        (org-publish-project-alist
+         `(("docs"
+            :time-stamp-file nil
+            :base-directory ,default-directory
+            :publishing-directory "/home/hinton/tmp/thermos-pdfs"
+            :recursive t
+            :publishing-function org-odt-publish-to-odt)
+           )))
+    (org-publish-project "docs" t)))
