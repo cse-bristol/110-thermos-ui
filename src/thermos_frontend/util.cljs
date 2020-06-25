580f66d1146920c64ee915bbbc8ca6d42fea0916
Tom Hinton <tom.hinton@cse.org.uk>
2020-06-25 11:06:36 +0100

    Add utility function to focus next element

--- src/thermos_frontend/util.cljs
+++ src/thermos_frontend/util.cljs
@@ -2,3 +2,23 @@
 
 (defn target-value [e]
   (.. e -target -value))
+
+(defn focus-next []
+  (let [candidates (->> (js/document.querySelectorAll
+                         "a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex=\"-1\"])")
+                        (js/Array.prototype.slice.call)
+                        (filter
+                         (fn [el]
+                           (or (pos? (.-offsetWidth el))
+                               (pos? (.-offsetHeight el))
+                               (= js/document.activeElement el)))))
+
+        target (->> (concat candidates [(first candidates)])
+                    (drop-while #(not= js/document.activeElement %))
+                    (second))
+        ]
+    (when target
+      (.focus target)
+      (.select target))))
+
+
