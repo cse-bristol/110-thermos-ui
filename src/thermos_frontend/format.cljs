94a5fe6257ceb692f92d6e6b24026cacce344c24
Tom Hinton <tom.hinton@cse.org.uk>
2020-08-19 16:32:24 +0100

    Add inverse of si-number, and make (si-number nil) => nil

--- src/thermos_frontend/format.cljs
+++ src/thermos_frontend/format.cljs
@@ -12,35 +12,50 @@
     (str (local-format (/ value scale)) " " prefix)))
 
 (defn si-number [value & [dimension]]
-  (let [sign (if (< value 0) "-" )
-        value (Math/abs value)
-        ]
-    (if dimension
-      (let [scale (case (aget dimension 0)
-                    "m" 0.001
-                    "c" 0.01
-                    "k" 1000
-                    "M" 1000000
-                    "G" 1000000000
-                    "T" 1000000000000)]
-        (str sign
-             (or (metric-prefix value (/ 1000000000000 scale) "T")
-                 (metric-prefix value (/ 1000000000 scale) "G")
-                 (metric-prefix value (/ 1000000 scale) "M")
-                 (metric-prefix value (/ 1000 scale) "k")
-                 (metric-prefix value scale "")
-                 (metric-prefix value (/ 0.001 scale) "m")
-                 (local-format value))
-             (.substring dimension 1)))
-      (str sign
-           (or (metric-prefix value 1000000000000 "T")
-               (metric-prefix value 1000000000 "G")
-               (metric-prefix value 1000000 "M")
-               (metric-prefix value 1000 "k")
-               (metric-prefix value 1 "")
-               (metric-prefix value 0.001 "m")
-               (local-format value))))))
+  (and value
+       (let [sign (if (< value 0) "-" )
+             value (Math/abs value)
+             ]
+         (if dimension
+           (let [scale (case (aget dimension 0)
+                         "m" 0.001
+                         "c" 0.01
+                         "k" 1000
+                         "M" 1000000
+                         "G" 1000000000
+                         "T" 1000000000000)]
+             (str sign
+                  (or (metric-prefix value (/ 1000000000000 scale) "T")
+                      (metric-prefix value (/ 1000000000 scale) "G")
+                      (metric-prefix value (/ 1000000 scale) "M")
+                      (metric-prefix value (/ 1000 scale) "k")
+                      (metric-prefix value scale "")
+                      (metric-prefix value (/ 0.001 scale) "m")
+                      (local-format value))
+                  (.substring dimension 1)))
+           (str sign
+                (or (metric-prefix value 1000000000000 "T")
+                    (metric-prefix value 1000000000 "G")
+                    (metric-prefix value 1000000 "M")
+                    (metric-prefix value 1000 "k")
+                    (metric-prefix value 1 "")
+                    (metric-prefix value 0.001 "m")
+                    (local-format value)))))))
 
 (def seconds util/format-seconds)
+(def scale
+  {"T" 1000000000000
+   "G" 1000000000
+   "M" 1000000
+   "k" 1000
+   "m" 0.001})
 
-
+(defn parse-si-number [t]
+  (try
+    (let [n (js/parseFloat t)
+          s (last t)]
+      (and (js/isFinite n)
+           (if-let [scale (scale s)]
+             (* n scale)
+             n)))
+    (catch :default e nil)))
