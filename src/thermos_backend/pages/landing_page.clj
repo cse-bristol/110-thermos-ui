0cca0a6a3fc26500e1bbbc7b5317a18cc5c2fe9a
Tom Hinton <tom.hinton@cse.org.uk>
2020-10-27 15:06:17 +0000

    Make the changelog message more annoying

--- src/thermos_backend/pages/landing_page.clj
+++ src/thermos_backend/pages/landing_page.clj
@@ -18,29 +18,7 @@
   (page
    {:title (str "Welcome, " (:name user))}
    [:div
-    (let [last-log (:changelog-seen user 0)
-          max-log (count changelog)
-          new-items (- max-log last-log)
-          changelog (take new-items changelog)]
-      (when (seq changelog)
-        [:div.card
-         [:div.flex-cols
-          [:h1 "THERMOS updated"]
-          [:a.button {:href "/help/changelog"
-                      :style {:margin-left "auto"}}
-           "🛈 Details"]
-          [:a.button {:href (str "?changes=" max-log)
-                      :style {:margin-left "1em"}}
-           "Close"]]
-         
-         [:p {:style {:margin-bottom 0}}
-          "Now featuring: "
-          (interpose " • "
-                     (for [rel changelog
-                           change (:changes rel)]
-                       (:title change)
-                       ))]
-         ]))
+    
     
     
     [:div.flex-cols.card
@@ -58,7 +36,38 @@
                    }}
      (for [project projects]
        (project-card project)
-       )
-     ]
+       )]
+
+    (let [last-log (:changelog-seen user 0)
+          max-log (count changelog)
+          new-items (- max-log last-log)
+          changelog (take new-items changelog)]
+      (when (seq changelog)
+        [:div {:style {:position "fixed"
+                       :background "rgba(1,1,1,0.5)"
+                       :left 0
+                       :top 0
+                       :width "100%"
+                       :height "100%"
+                       }}
+         [:div.card {:style {:margin "5em"}}
+          [:div.flex-cols
+           [:h1 "THERMOS has been updated"]
+           [:a.button {:href "/help/changelog"
+                       :target "help"
+                       :style {:margin-left "auto"}}
+            "🛈 More details"]
+           [:a.button {:href (str "?changes=" max-log)
+                       :style {:margin-left "1em"}}
+            "Close"]]
+          
+          [:p {:style {:margin-bottom 0}}
+           "Now featuring: "
+           (interpose " • "
+                      (for [rel changelog
+                            change (:changes rel)]
+                        (:title change)
+                        ))]]
+         ]))
     ]
    ))
