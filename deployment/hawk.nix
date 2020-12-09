2e4ac6942d65b10bcc8e7e46a36bbea44c89bb80
Tom Hinton <tom.hinton@cse.org.uk>
2020-12-09 12:53:12 +0000

    Add temporary deployment for HAWK

---
+++ deployment/hawk.nix
@@ -0,0 +1,33 @@
+{
+  network.name = "Hawk interactive deployment";
+
+  thermos-ui = {config, ...} : {
+    imports = [ ./thermos.nix ];
+    
+    deployment.digitalOcean.size = "c-32";
+    deployment.digitalOcean.region = "lon1";
+    deployment.digitalOcean.image = "nixos20.09";
+    deployment.sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuI2T0KT2g5Z7xDfA36eypCTVUW+AZA6Q7/0Hvvdm3wqSyYHokw1Y7pLv/9+K/VL35vpfN368f5FAlbqYiM0p4UKrM7RgtgkyC77xg2ZXnJYNCwLHMph/GdteTgg/fBWXIzFeTMQCME3pILVhCYuQL2qJm0diihf7zuI1r+jxdwhMjY6BNNgfJ6SY1LZ7p0p9zGdGJGypEuGDFAhP5sGIiypLSpl/C0GOpYJzKUWR8MzMPsbfK/klpKS+AXGwmr27s8VmmzYFzueFExiFgDQ9N8EWO/XMtse1d4CLe+4tPYHzIqrIPdZXh11TdcmUL8g/lykOufPeLNmvXtEXCXxQx cse-server-root-key";
+
+    deployment.keys.smtp.keyFile = ./smtp-password;
+
+    services.thermos.ui.enable = true;
+    services.thermos.model.enable = true;
+    services.thermos.importer.enable = true;
+
+    nixpkgs.config.allowUnfree = true;
+    
+    services.thermos.ui.baseUrl = "http://hawk.thermos-project.eu";
+    services.thermos.model.solverCount = 32;
+
+    networking.firewall.allowedTCPPorts = [ 80 ];
+
+    services.nginx = {
+      enable = true;
+      clientMaxBodySize = "10g";
+      virtualHosts."hawk-tool.thermos-project.eu" = {
+        locations."/" = { proxyPass = "http://localhost:${toString config.services.thermos.ui.port}/"; };
+      };
+    };
+  };
+}
