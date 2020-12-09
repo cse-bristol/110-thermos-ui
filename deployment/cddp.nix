0966073fa856c18b1fe28ec07634dd04e576e34d
Tom Hinton <tom.hinton@cse.org.uk>
2020-12-09 12:52:50 +0000

    Add ACME to CDDP deployment file

--- deployment/cddp.nix
+++ deployment/cddp.nix
@@ -6,7 +6,7 @@
     
     deployment.digitalOcean.size = "c-32";
     deployment.digitalOcean.region = "lon1";
-    deployment.digitalOcean.image = "nixos-20.09";
+    deployment.digitalOcean.image = "nixos20.09";
     deployment.sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuI2T0KT2g5Z7xDfA36eypCTVUW+AZA6Q7/0Hvvdm3wqSyYHokw1Y7pLv/9+K/VL35vpfN368f5FAlbqYiM0p4UKrM7RgtgkyC77xg2ZXnJYNCwLHMph/GdteTgg/fBWXIzFeTMQCME3pILVhCYuQL2qJm0diihf7zuI1r+jxdwhMjY6BNNgfJ6SY1LZ7p0p9zGdGJGypEuGDFAhP5sGIiypLSpl/C0GOpYJzKUWR8MzMPsbfK/klpKS+AXGwmr27s8VmmzYFzueFExiFgDQ9N8EWO/XMtse1d4CLe+4tPYHzIqrIPdZXh11TdcmUL8g/lykOufPeLNmvXtEXCXxQx cse-server-root-key";
 
     deployment.keys.smtp.keyFile = ./smtp-password;
@@ -17,14 +17,19 @@
 
     nixpkgs.config.allowUnfree = true;
     
-    services.thermos.ui.baseUrl = "https://cddp-thermos.cse.org.uk/";
+    services.thermos.ui.baseUrl = "https://cddp-thermos.cse.org.uk";
 
-    networking.firewall.allowedTCPPorts = [ 80 ];
+    networking.firewall.allowedTCPPorts = [ 80 443 ];
 
+    security.acme.acceptTerms = true;
+    security.acme.email = "tom.hinton@cse.org.uk";
+    
     services.nginx = {
       enable = true;
-
+      clientMaxBodySize = "10g";
       virtualHosts."cddp-thermos.cse.org.uk" = {
+        forceSSL = true;
+        enableACME = true;
         locations."/" = { proxyPass = "http://localhost:${toString config.services.thermos.ui.port}/"; };
       };
     };
