f18a22d04fde6ef8eb9a8f40bf952661873a68f6
Tom Hinton <tom.hinton@cse.org.uk>
2021-01-27 20:33:31 +0000

    Render paths at wider zoom; render solution at even wider zoom

--- src/thermos_frontend/tile.cljs
+++ src/thermos_frontend/tile.cljs
@@ -99,19 +99,28 @@
       ;; Selected buildings
       (doseq [candidate selected-buildings]
         (render-candidate zoom has-solution? candidate ctx project map-view))
-      (when (> zoom 15)
-        ;; Non-selected paths
-        (doseq [path non-selected-paths]
-          (let [line-width (pipe-diam-line-width path)]
-            (render-candidate zoom has-solution? path ctx project map-view line-width)))
-        ;; Selected path shadows
-        (doseq [path selected-paths]
-          (let [line-width (pipe-diam-line-width path)]
-            (render-candidate-shadow zoom has-solution? path ctx project  map-view line-width)))
-        ;; Selected paths
-        (doseq [path selected-paths]
-          (let [line-width (pipe-diam-line-width path)]
-            (render-candidate zoom has-solution? path ctx project map-view line-width)))))
+
+      (if (> zoom 13)
+        (do
+          ;; Non-selected paths
+          (doseq [path non-selected-paths]
+            (let [line-width (pipe-diam-line-width path)]
+              (render-candidate zoom has-solution? path ctx project map-view line-width)))
+          ;; Selected path shadows
+          (doseq [path selected-paths]
+            (let [line-width (pipe-diam-line-width path)]
+              (render-candidate-shadow zoom has-solution? path ctx project  map-view line-width)))
+          ;; Selected paths
+          (doseq [path selected-paths]
+            (let [line-width (pipe-diam-line-width path)]
+              (render-candidate zoom has-solution? path ctx project map-view line-width))))
+        
+        (when has-solution?
+          (doseq [path non-selected-paths]
+            (when candidate/in-solution?
+              (let [line-width (pipe-diam-line-width path)]
+                (render-candidate zoom has-solution? path ctx project map-view line-width)))))
+        ))
     ))
 
 (defn render-candidate
