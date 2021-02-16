1323183c05bb5f89157a4facf728f19e6dddcc75
Neil Justice <neil.justice@cse.org.uk>
2021-02-16 16:05:10 +0000

    added map of LIDAR to main 'manage LIDAR' page

--- src/thermos_backend/pages/lidar.clj
+++ src/thermos_backend/pages/lidar.clj
@@ -4,10 +4,20 @@
 
 (defn manage-lidar [project-name lidar-info]
   (page
-   {:title "Manage LIDAR"}
+   {:title "Manage LIDAR"
+    :js ["/js/manage-lidar.js"]
+    :css ["/css/manage-lidar.css"]}
    [:div
     [:div.card {:style {:margin-bottom "2em"}}
-     [:h1 {:style {:margin-bottom "2em"}} "Manage LIDAR for project " project-name]
+     [:h1 "Manage LIDAR for project " project-name]
+     [:p "LIDAR data is used to calculate building heights and volumes for heat demand estimation.
+          LIDAR data is only used during map import, and adding or deleting LIDAR will not affect
+          any existing maps or networks."]
+     [:p "LIDAR coverage is not mandatory, but building height data of some sort will 
+          improve the quality of any demand estimates produced from the built-in regression model.
+          This can be from a field on the building polygons, or from OpenStreetMap if that
+          is the source of buildings. OpenStreetMap does not always contain height data."]
+     [:div#manage-lidar-map {:style {:width "100%" :height "500px" :margin-bottom "2em"}}]
      [:table {:style {:width "100%" :margin-bottom "2em"}}
       [:thead
        [:tr (map (fn [header] [:th header]) ["File name" "Bounds" "CRS" "Download" "Delete"])]]
@@ -22,7 +32,7 @@
                      [:td [:form {:method :GET :action filename}
                            [:button.button {:type :submit :value "DOWNLOAD"} "DOWNLOAD"]]]
                      [:td [:a.button.button--danger {:href (str "delete/" filename)} "DELETE"]]])) 
-        lidar-info)]]
+        (sort-by :filename lidar-info))]]
      [:div
       [:form#upload-form {:method :POST :enctype "multipart/form-data"}
        [:label.button [:input {:value ""
