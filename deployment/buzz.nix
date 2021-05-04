27cc151b592168fa2fa6a97ee3dbf73ce58bbe0b
Tom Hinton <tom.hinton@cse.org.uk>
2021-05-04 09:54:16 +0100

    Update buzz deployment to have backup to s3

--- deployment/buzz.nix
+++ deployment/buzz.nix
@@ -1,6 +1,33 @@
 {
   thermos-app = {pkgs, config, ...}:
-  { deployment.targetEnv = "container";
+  {
+    deployment.targetEnv = "container";
     deployment.keys.smtp.keyFile = ./smtp-password;
+    deployment.keys.spaces-access-key.keyFile = ./spaces-access-key;
+    deployment.keys.spaces-secret-key.keyFile = ./spaces-secret-key;
+    deployment.keys.backup-file-name.keyFile = ./backup-file-name;
+
+    # Backup postgres to digitalocean S3
+    services.postgresqlBackup.enable = true;
+
+    systemd.services.uploadBackup = {
+      path = [ pkgs.s3cmd ];
+      script = ''
+        s3cmd --access-key=$(cat /run/keys/spaces-access-key) \
+              --secret-key=$(cat /run/keys/spaces-secret-key) \
+              --host ams3.digitaloceanspaces.com \
+              --host-bucket='%(bucket)s.ams3.digitaloceanspaces.com' \
+           put ${config.services.postgresqlBackup.location}/all.sql.gz \
+           s3://thermos-backup/$(cat /run/keys/backup-file-name)-$(date +%a)
+      '';
+      
+      # yuck
+      after = [
+        "postgresqlBackup.service"
+      ];
+      wantedBy = [
+        "postgresqlBackup.service"
+      ];
     };
+  };
 }
